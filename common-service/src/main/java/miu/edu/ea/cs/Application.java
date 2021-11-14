6c4ac04ab32bc213084c52395e4705661223a649
Faisal Ahmed <faisalbegins@gmail.com>
2021-11-14 14:35:20 -0600

    authentication initial commit
    added auth server and resource server POC

--- common-service/src/main/java/miu/edu/ea/cs/Application.java
+++ common-service/src/main/java/miu/edu/ea/cs/Application.java
@@ -3,8 +3,16 @@ package miu.edu.ea.cs;
 import edu.miu.ea.cm.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableResourceServer
 public class Application {
 
     public static void main(String[] args) {
@@ -12,5 +20,17 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 
+    @Bean
+    public ResourceServerTokenServices tokenService() {
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setClientId("b");
+        tokenServices.setClientSecret("b");
+        tokenServices.setCheckTokenEndpointUrl("http://localhost:8080/oauth/check_token");
+        return tokenServices;
+    }
 }
