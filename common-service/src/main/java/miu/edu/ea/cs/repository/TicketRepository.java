86b27a51d78fc31fc9aaa87956a40754087189f2
Weldmicheal Berhanu <michock.mit@gmail.com>
2021-11-15 16:15:21 -0600

    ticket service is done

--- common-service/src/main/java/miu/edu/ea/cs/repository/TicketRepository.java
+++ common-service/src/main/java/miu/edu/ea/cs/repository/TicketRepository.java
@@ -1,6 +1,9 @@
 package miu.edu.ea.cs.repository;
 
+import edu.miu.ea.cm.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TicketRepository extends JpaRepository {
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
--- common-service/src/main/java/miu/edu/ea/cs/services/servicesImpl/TicketServiceImpl.java
+++ common-service/src/main/java/miu/edu/ea/cs/services/servicesImpl/TicketServiceImpl.java
@@ -1,4 +1,26 @@
 package miu.edu.ea.cs.services.servicesImpl;
 
-public class TicketServiceImpl {
+import edu.miu.ea.cm.Ticket;
+import miu.edu.ea.cs.repository.TicketRepository;
+import miu.edu.ea.cs.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+    @Autowired
+    private TicketRepository repository;
+
+    public Page<Ticket> allTickets(Pageable pageable){
+        return repository.findAll(pageable);
+    }
+
+    public Ticket saveTicket(Ticket ticket) {
+
+        return repository.save(ticket);
+    }
+
 }
